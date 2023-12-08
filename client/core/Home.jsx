d78d8402e6614ed1260e508e47b5b05f2377e861
winghk00 <146040912+winghk00@users.noreply.github.com>
2023-12-08 17:57:37 -0500

    v0.5c changed homepage -Kwok Wing

--- client/core/Home.jsx
+++ client/core/Home.jsx
@@ -7,12 +7,14 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
+import Chip from '@material-ui/core/Chip';
 import Paper from "@material-ui/core/Paper";
 import { Link as RouterLink } from "react-router-dom";
 import Link from "@material-ui/core/Link";
 import Box from "@material-ui/core/Box";
 import ImageList from "@material-ui/core/ImageList";
 import Typography from "@material-ui/core/Typography";
+import FaceIcon from '@material-ui/icons/Face';
 
 import bookImg from './../assets/images/book.png';
 import weblogo from './../assets/images/logo.png';
@@ -40,6 +42,11 @@ const useStyles = makeStyles((theme) => ({
   },
   root: {
     // Define your root styles here
+  },
+  media: {           // this is the`className` passed to `CardMedia` later
+    //height: 100,     // as an example I am modifying width and height
+    width: 250,
+    //marginLeft: '-33%'
   },
 }));
 
@@ -88,30 +95,30 @@ export default function Lists() {
   const classes = useStyles();
   return (
     <Paper className={classes.root} elevation={4}>
-          <Card className={classes.card}>
-    <Typography variant="h4" className={classes.title}>Welcome to Web Ninja's Library</Typography>
+    <Card className={classes.card}>
+      <Typography variant="h4" className={classes.title}>Welcome to Web Ninja's Library</Typography>
     </Card>
     <Typography variant="h4" className={classes.title}>Collections of books</Typography>
-      <ImageList sx={{ width: 500, margin: "10px" }} variant="quilted" cols={3} rowHeight={320} >
+      <ImageList sx={{ width: 500, margin: "10px" }} variant="quilted" cols={4} rowHeight={520} >
         {books.map((item, i) => {
           return (
             <Link component={RouterLink} to={"/bookPub/" + item._id} key={i}>
-                            <Card sx={{ maxWidth: 345 }}>
+              <Card variant="outlined"
+      orientation="horizontal" sx={{ maxWidth: 345 }}>
                 <CardMedia
+                  className={classes.media}
                   component="img"
                   alt={item.title}
-                  height="200"
+                  //height="200"
                   //image={`${bookImg}`}
                   image={`${item.coverImg}`}
                   sx={{ padding: "1em 1em 0 1em", objectFit: "contain" }}
                 />
                 <CardContent>
-                  <Typography gutterBottom variant="h6" component="div">
+                  <Typography gutterBottom  variant="h6" component="div">
                     {item.title}
                   </Typography>
-                  <Typography variant="body2" color="text.secondary">
-                    Author: {item.author}
-                  </Typography>
+                  <Chip variant="outlined" label={`Author: ${item.author}`} icon={<FaceIcon />} />
                 </CardContent>
               </Card>
                 
